08d7fc92cdf45f54e35f65a50daa480cf7224ab6
Parv Daga <123968142+Parvdaga@users.noreply.github.com>
2023-10-09 23:45:42 +0530

    custom.py

--- custom.py
+++ custom.py
@@ -4,14 +4,14 @@ import json
 import numpy as np
 import skimage.draw
 import cv2
-from mrcnn.visualize import display_instances
-import matplotlib.pyplot as plt
+import imgaug
 
 # Root directory of the project
 ROOT_DIR = "C:/Users/parvd/OneDrive/Desktop/Project/Project"
 
-# Import Mask RCNN
-sys.path.append(ROOT_DIR)  # To find local version of the library
+# Import Mask RCNN (assuming Mask R-CNN is in the ROOT_DIR)
+sys.path.append(ROOT_DIR)  
+
 from mrcnn.config import Config
 from mrcnn import model as modellib, utils
 
@@ -24,13 +24,16 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 class CustomConfig(Config):
     NAME = "object"
+    GPU_COUNT = 1
     IMAGES_PER_GPU = 1
-    NUM_CLASSES = 1 + 28
-    STEPS_PER_EPOCH = 10
+    NUM_CLASSES = 1 + 4  # Background + Apple_Black_rot, Apple_healthy, Apple_Rust, Apple_Scrab
+    STEPS_PER_EPOCH = 5
     DETECTION_MIN_CONFIDENCE = 0.9
+    LEARNING_RATE = 0.001
 
 class CustomDataset(utils.Dataset):
     def load_custom(self, dataset_dir, subset):
+        # Add classes
         self.add_class("object", 1, "Apple_Black_Rot")
         self.add_class("object", 2, "Apple_Healthy")
         self.add_class("object", 3, "Apple_Rust")
@@ -39,7 +42,8 @@ class CustomDataset(utils.Dataset):
         assert subset in ["train", "val"]
         dataset_dir = os.path.join(dataset_dir, subset)
 
-        annotations1 = json.load(open(os.path.join(dataset_dir, 'Apple.json')))
+        # Load annotations
+        annotations1 = json.load(open(os.path.join(dataset_dir, "Apple.json")))
         annotations = list(annotations1.values())
 
         annotations = [a for a in annotations if a['regions']]
@@ -47,7 +51,6 @@ class CustomDataset(utils.Dataset):
         for a in annotations:
             polygons = [r['shape_attributes'] for r in a['regions']]
             objects = [s['region_attributes']['names'] for s in a['regions']]
-            print("objects:", objects)
             name_dict = {"Apple_Black_Rot": 1, "Apple_Healthy": 2, "Apple_Rust": 3, "Apple_Scab": 4}
             num_ids = [name_dict[a] for a in objects]
 
@@ -66,16 +69,9 @@ class CustomDataset(utils.Dataset):
 
     def load_mask(self, image_id):
         image_info = self.image_info[image_id]
-        if image_info["source"] != "object":
-            return super(self.__class__, self).load_mask(image_id)
-
-        info = self.image_info[image_id]
-        if info["source"] != "object":
-            return super(self.__class__, self).load_mask(image_id)
-        num_ids = info['num_ids']
-        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
-                        dtype=np.uint8)
-        for i, p in enumerate(info["polygons"]):
+        num_ids = image_info['num_ids']
+        mask = np.zeros([image_info["height"], image_info["width"], len(image_info["polygons"])], dtype=np.uint8)
+        for i, p in enumerate(image_info["polygons"]):
             rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
             mask[rr, cc, i] = 1
 
@@ -98,23 +94,33 @@ def train(model):
     dataset_val.load_custom("C:/Users/parvd/OneDrive/Desktop/Project/Project/Data Set/Val", "val")
     dataset_val.prepare()
 
-    print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=100,
-                layers='heads')
+                epochs=300,
+                layers='heads',
+                augmentation=imgaug.augmenters.Sequential([
+                    imgaug.augmenters.Fliplr(1),
+                    imgaug.augmenters.Flipud(1),
+                    imgaug.augmenters.Affine(rotate=(-45, 45)),
+                    imgaug.augmenters.Affine(rotate=(-90, 90)),
+                    imgaug.augmenters.Affine(scale=(0.5, 1.5)),
+                    imgaug.augmenters.Crop(px=(0, 10)),
+                    imgaug.augmenters.Grayscale(alpha=(0.0, 1.0)),
+                    imgaug.augmenters.AddToHueAndSaturation((-20, 20)),
+                    imgaug.augmenters.Add((-10, 10), per_channel=0.5),
+                    imgaug.augmenters.Invert(0.05, per_channel=True),
+                    imgaug.augmenters.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),
+                ]))
 
 config = CustomConfig()
-model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=DEFAULT_LOGS_DIR)
+model = modellib.MaskRCNN(mode="training", config=config, model_dir=DEFAULT_LOGS_DIR)
 
 weights_path = COCO_WEIGHTS_PATH
-
 if not os.path.exists(weights_path):
     utils.download_trained_weights(weights_path)
 
 model.load_weights(weights_path, by_name=True, exclude=[
-            "mrcnn_class_logits", "mrcnn_bbox_fc",
-            "mrcnn_bbox", "mrcnn_mask"])
+    "mrcnn_class_logits", "mrcnn_bbox_fc",
+    "mrcnn_bbox", "mrcnn_mask"])
 
 train(model)
